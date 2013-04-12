33a1b3fe9f4fc7ba1798eb5af3cf54513c0fcc7a
James Louie <j-louie@live.com>
2013-04-11 23:53:04 -0700

    Temporary World -> level interaction created for minh to work on the level design.

--- QuickMatch/gen/com/example/quickmatch/R.java
+++ QuickMatch/gen/com/example/quickmatch/R.java
@@ -37,13 +37,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int World_Screen=0x7f070009;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
-        public static final int profile1=0x7f070005;
-        public static final int profile2=0x7f070006;
-        public static final int profile3=0x7f070007;
-        public static final int profile_text=0x7f070004;
-        public static final int profile_title=0x7f070003;
+        public static final int level1=0x7f070003;
+        public static final int profile1=0x7f070006;
+        public static final int profile2=0x7f070007;
+        public static final int profile3=0x7f070008;
+        public static final int profile_text=0x7f070005;
+        public static final int profile_title=0x7f070004;
         public static final int textView1=0x7f070002;
     }
     public static final class layout {

7de03c839691c4e51ce66621dcbdd187dcfa2254
MartianMan92 <martian.man92@yahoo.com>
2013-04-13 01:40:54 -0700

    Made extensive changes to the DB, most noticeable is created the initial table statements.

--- QuickMatch/src/com/example/quickmatch/QuickMatchDatabase.java
+++ QuickMatch/src/com/example/quickmatch/QuickMatchDatabase.java
@@ -5,10 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class QuickMatchDatabase {
-
+	private static final String TAG = "QuickMatchOpenHelper";
 	private static String DATABASE_NAME = "quickMatchDB";
 	private final QuickMatchOpenHelper qmDatabaseOpenHelper;
-	
+    private static final int DATABASE_VERSION = 2;
+    
 	public QuickMatchDatabase(Context context) {
         qmDatabaseOpenHelper = new QuickMatchOpenHelper(context);
     }
@@ -17,31 +18,55 @@ public class QuickMatchDatabase {
 	//Helps run DB commands
 	private static class QuickMatchOpenHelper extends SQLiteOpenHelper {
 
-		private static final String TAG = "QuickMatchOpenHelper";
-		private static Boolean initializedDB;
-	    private static final int DATABASE_VERSION = 2;
-	    //Profile Table Build Contents
+        private final Context mHelperContext;
+        private SQLiteDatabase mDatabase;
+        
+	    //Profile Table Columns & Build
 	    private static final String PROFILE_TABLE_NAME = "quick_match";
-		private static String USERNAME;
-		private static int CURRENTQUOTA;
-		private static String PICNAME;
+	    private static String PROFILEID = "profile_id";
+		private static String USERNAME = "username";
+		private static String CURRENTQUOTA = "current_quota";
+		private static String PICNAME = "pic_name";
 	    private static final String PROFILE_TABLE_CREATE =
-	                "CREATE TABLE " + PROFILE_TABLE_NAME + " (" +
+	                "CREATE TABLE " + PROFILE_TABLE_NAME + " (" + PROFILEID + " INTEGER, " +
 	                USERNAME + " TEXT, " +
-	                CURRENTQUOTA + " INT, " + PICNAME + " TEXT);"
+	                CURRENTQUOTA + " INT, " + PICNAME + " TEXT, PRIMARY KEY (" + PROFILEID + "ASC));"
 	                ;
-	    //World Table Build Contents
-	    //Level Table Build Contents
-
+	    //World Table Columns & Build
+	    public static final String WORLD_TABLE_NAME = "world";
+	    private static String WORLDID = "world_id";
+	    private static String WORLDNAME = "world_name";
+	    private static String WORLDQUOTA = "current_world_quota";
+	    private static String WORLDUNLOCKED = "unlocked";
+	    private static final String WORLD_TABLE_CREATE =
+                "CREATE TABLE " + WORLD_TABLE_NAME + " (" +
+                WORLDID + " INTEGER, " + WORLDQUOTA + " INT, " +
+                WORLDNAME + " TEXT, " + WORLDUNLOCKED + " TEXT, PRIMARY KEY(" + WORLDID + " ASC));"
+                ;
+	    //Level Table Columns & Build
+	    public static final String LEVEL_TABLE_NAME = "level";
+	    private static String LEVELID = "level_id";
+	    private static String LEVELNAME = "level_name";
+	    private static String LEVELQUOTA = "current_level_quota";
+	    private static String LVLUNLOCKED = "unlocked";
+	    private static final String LEVEL_TABLE_CREATE =
+                "CREATE TABLE " + LEVEL_TABLE_NAME + " (" + WORLDID + " INTEGER, " +
+                LEVELID + " INT, " + LEVELQUOTA + " INT, " +
+                LEVELNAME + " TEXT, " + LVLUNLOCKED + " TEXT, FOREIGN KEY(" + WORLDID + ") REFERENCES +" +
+                		WORLD_TABLE_NAME + "(" + WORLDID + "));"
+                ;
+	    
 	    QuickMatchOpenHelper(Context context) {
 	        super(context, DATABASE_NAME , null, DATABASE_VERSION);
+	        mHelperContext = context;
 	    }
 
 	    @Override
 	    public void onCreate(SQLiteDatabase db) {
-	    	initializedDB = true;
+	    	mDatabase = db;
 	        db.execSQL(PROFILE_TABLE_CREATE);
-	        
+	        db.execSQL(WORLD_TABLE_CREATE);
+	        db.execSQL(LEVEL_TABLE_CREATE);
 	    }
 
 		@Override
